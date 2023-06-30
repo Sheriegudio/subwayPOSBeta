c79e0d867130046dc1217bb70433df5f3dd9749a
N01579863 <N01579863@HCHUMBE-LPPOJPE>
2023-06-29 21:49:56 -0400

    Login page with all functionalities and validations

---
+++ SubwayPOS/Login.Designer.cs
@@ -0,0 +1,93 @@
+﻿using System.Windows.Forms;
+
+namespace SubwayPOS
+{
+    partial class Login
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            txt_userId = new TextBox();
+            txt_password = new TextBox();
+            lbl_userId = new Label();
+            lbl_psword = new Label();
+            btn_Login = new Button();
+            SuspendLayout();
+            // 
+            // txt_userId
+            // 
+            resources.ApplyResources(txt_userId, "txt_userId");
+            txt_userId.Name = "txt_userId";
+            // 
+            // txt_password
+            // 
+            resources.ApplyResources(txt_password, "txt_password");
+            txt_password.Name = "txt_password";
+            // 
+            // lbl_userId
+            // 
+            resources.ApplyResources(lbl_userId, "lbl_userId");
+            lbl_userId.Name = "lbl_userId";
+            // 
+            // lbl_psword
+            // 
+            resources.ApplyResources(lbl_psword, "lbl_psword");
+            lbl_psword.Name = "lbl_psword";
+            // 
+            // btn_Login
+            // 
+            btn_Login.BackColor = System.Drawing.Color.LightSteelBlue;
+            resources.ApplyResources(btn_Login, "btn_Login");
+            btn_Login.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            btn_Login.Name = "btn_Login";
+            btn_Login.UseVisualStyleBackColor = false;
+            btn_Login.Click += btn_Login_Click;
+            // 
+            // Login
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(btn_Login);
+            Controls.Add(lbl_psword);
+            Controls.Add(lbl_userId);
+            Controls.Add(txt_password);
+            Controls.Add(txt_userId);
+            Name = "Login";
+            Load += Login_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txt_userId;
+        private TextBox txt_password;
+        private Label lbl_userId;
+        private Label lbl_psword;
+        private Button btn_Login;
+    }
+}
---
+++ SubwayPOS/Login.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SubwayPOS
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        public string connectionString = "Data Source=(localdb)\\ProjectModels;Initial Catalog=Subway;Integrated Security=True";
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+            SqlCommand cmd = new SqlCommand(" SELECT USERID, USERNAME FROM USERS WHERE USERID='26' ", con);
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.Read() && reader["USERID"].ToString().Equals("26"))
+            {
+
+            }
+            con.Close();
+        }
+
+        private void btn_Login_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int userId;
+                userId = int.TryParse(txt_userId.Text, out userId) ? int.Parse(txt_userId.Text) : throw new FormatException("userId");
+                int passWord;
+                passWord = int.TryParse(txt_password.Text, out passWord) ? int.Parse(txt_password.Text) : throw new FormatException("passWord");
+
+
+                //QUERY TO AUTHENTICATE USER
+                string authenticationQurery = "SELECT USERID, USERNAME, PASSWORD FROM USERS " +
+                    "WHERE USERID = @ID AND PASSWORD = @PASSWORD";
+
+                SqlConnection con = new SqlConnection(connectionString);
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand(authenticationQurery, con);
+                cmd.Parameters.AddWithValue("ID", userId);
+                cmd.Parameters.AddWithValue("PASSWORD", passWord);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    MessageBox.Show("Success");
+                }
+                else
+                {
+                    txt_userId.Text = "";
+                    txt_password.Text = "";
+                    MessageBox.Show("Incorrect User Id or Password.", "Access Denied", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                con.Close();
+
+            }
+            catch (System.FormatException ex)
+            {
+                if (ex.Message.Equals("userId"))
+                {
+                    txt_userId.Text = "";
+                    txt_userId.Focus();
+                    MessageBox.Show("Please enter valid numeric value for User ID.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                else if (ex.Message.Equals("passWord"))
+                {
+                    txt_password.Text = "";
+                    txt_userId.Focus();
+                    MessageBox.Show("Please enter valid numeric value for Password.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+        }
+    }
+}
