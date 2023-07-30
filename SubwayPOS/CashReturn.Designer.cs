4e8697879053e4101a03ed22de8115f1853685ac
Sheriegudio <139591905+Sheriegudio@users.noreply.github.com>
2023-07-30 14:14:49 -0400

    Add files via upload

---
+++ SubwayPOS/CashReturn.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace SubwayPOS
+{
+    partial class CashReturn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CashReturn));
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.txtCash = new System.Windows.Forms.TextBox();
+            this.txtUserName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(160, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(304, 68);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Return Cash";
+            // 
+            // btnOK
+            // 
+            this.btnOK.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnOK.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnOK.Location = new System.Drawing.Point(216, 234);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(155, 74);
+            this.btnOK.TabIndex = 10;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = false;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // txtCash
+            // 
+            this.txtCash.Font = new System.Drawing.Font("Verdana", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtCash.Location = new System.Drawing.Point(196, 147);
+            this.txtCash.Name = "txtCash";
+            this.txtCash.Size = new System.Drawing.Size(226, 71);
+            this.txtCash.TabIndex = 11;
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.AutoSize = true;
+            this.txtUserName.Font = new System.Drawing.Font("Verdana", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtUserName.Location = new System.Drawing.Point(12, 19);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(122, 26);
+            this.txtUserName.TabIndex = 12;
+            this.txtUserName.Text = "userName";
+            // 
+            // CashReturn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 365);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.txtCash);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "CashReturn";
+            this.Text = "CashReturn";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.TextBox txtCash;
+        private System.Windows.Forms.Label txtUserName;
+    }
+}
--- SubwayPOS/Model.cs
+++ SubwayPOS/Model.cs
@@ -9,7 +9,7 @@ namespace SubwayPOS
 {
     public class Model
     {
-        public static string connectionString = "Data Source=(localdb)\\ProjectModels;Initial Catalog=Subway;Integrated Security=True";
+        public static string connectionString = "Data Source=DESKTOP-H6LMUBJ;Initial Catalog=Subway;Integrated Security=True";
         public static SqlConnection con;
         public static SqlDataReader authenticateUser(int userId, int passWord)
         {
@@ -26,6 +26,24 @@ namespace SubwayPOS
             //closing connection
             return reader;
         }
+
+        public static SqlDataReader insertUser(int userId, int passWord, String userName)
+        {
+            //opening conncetion
+            openConnection();
+
+            //logic for autnemtication
+            string authenticationQurery = $"insert into USERS (USERID,PASSWORD,USERNAME) VALUES(@ID,@PASSWORD,@USERNAME)";
+            SqlCommand cmd = new SqlCommand(authenticationQurery, con);
+            cmd.Parameters.AddWithValue("ID", userId);
+            cmd.Parameters.AddWithValue("PASSWORD", passWord);
+            cmd.Parameters.AddWithValue("USERNAME", userName);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            //closing connection
+            return reader;
+        }
+
         public static void openConnection()
         {
             con = new SqlConnection(connectionString);
